d32bdb2443d45adf186ba6aed5d496277a504b30
axynon <axynonone@gmail.com>
2023-12-02 19:04:39 +0500

    1

--- 64b157ddd4194a2c24aa1cd4/js/webflow.ec9dea089.js
+++ 64b157ddd4194a2c24aa1cd4/js/webflow.ec9dea089.js
@@ -1,10 +1,6 @@
 
 /*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
+Let is begin
  */
 
 (() => {
